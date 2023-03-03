57260d28cd01f5cbadeeaab9ce286995a6db95df
Neil Chen <neilchennc@gmail.com>
2023-03-03 06:49:50 +0000

    Using filp->private_data to access device data instead

--- driver/neil-dev.c
+++ driver/neil-dev.c
@@ -10,7 +10,7 @@
  * Author: Neil Chen <neilchennc@gmail.com>
  * Github: https://github.com/neilchennc/agx-orin-power-button-detection
  * Created at: 2023-02-22
- * Version: 1.0
+ * Version: 1.1
  */
 
 #include <linux/init.h>
@@ -56,7 +56,9 @@ volatile static int irq_occurred_flag;
 
 static irqreturn_t irq_handler(int irq, void *dev_id)
 {
-	dev_info(device_data.drv_dev, "Interrupt #%d occurred\n", irq);
+	struct neil_device_data *p_data = dev_id;
+
+	dev_info(p_data->drv_dev, "interrupt #%d occurred\n", irq);
 
 	irq_occurred_flag = 1;
 
@@ -73,92 +75,103 @@ static int neil_uevent(struct device *dev, struct kobj_uevent_env *env)
 }
 #endif
 
-static int neil_open(struct inode *inode, struct file *file)
+static int neil_open(struct inode *inode, struct file *filp)
 {
-	dev_info(device_data.drv_dev, "device open\n");
+	struct neil_device_data *p_data;
+
+	p_data = container_of(inode->i_cdev, struct neil_device_data, cdev);
+	filp->private_data = p_data;
+
+	dev_info(p_data->drv_dev, "device open\n");
+
 	return 0;
 }
 
-static int neil_release(struct inode *inode, struct file *file)
+static int neil_release(struct inode *inode, struct file *filp)
 {
-	dev_info(device_data.drv_dev, "device close\n");
+	struct neil_device_data *p_data = filp->private_data;
+	dev_info(p_data->drv_dev, "device release\n");
 	return 0;
 }
 
-static long neil_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+static long neil_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
-	dev_info(device_data.drv_dev, "device ioctl\n");
+	struct neil_device_data *p_data = filp->private_data;
+	dev_info(p_data->drv_dev, "device ioctl\n");
 	return 0;
 }
 
-static ssize_t neil_read(struct file *file, char __user *buf, size_t count, loff_t *offset)
+static ssize_t neil_read(struct file *filp, char __user *buf, size_t count, loff_t *offset)
 {
+	struct neil_device_data *p_data = filp->private_data;
 	uint8_t *data = "Data from the kernel space";
 	size_t data_len = strlen(data);
 	size_t size_to_copy, n_copied;
 
-	dev_info(device_data.drv_dev, "read device, minor: %d, count: %ld\n", MINOR(file->f_path.dentry->d_inode->i_rdev), count);
+	dev_info(p_data->drv_dev, "read device, minor: %d, count: %ld\n", MINOR(filp->f_path.dentry->d_inode->i_rdev), count);
 
 	if (data_len > count)
 	{
 		size_to_copy = count;
-		dev_warn(device_data.drv_dev, "Only copy %ld bytes to user\n", size_to_copy);
+		dev_warn(p_data->drv_dev, "only copy %ld bytes to user\n", size_to_copy);
 	}
 	else
 	{
 		size_to_copy = data_len;
-		dev_info(device_data.drv_dev, "Copy %ld bytes to user\n", size_to_copy);
+		dev_info(p_data->drv_dev, "copy %ld bytes to user\n", size_to_copy);
 	}
 
 	n_copied = copy_to_user(buf, data, size_to_copy);
 	if (n_copied < 0)
 	{
-		dev_err(device_data.drv_dev, "copy_to_user failed\n");
-		return -EFAULT;
+		dev_err(p_data->drv_dev, "copy_to_user failed\n");
+		return -EACCES;
 	}
 
 	return size_to_copy;
 }
 
-static ssize_t neil_write(struct file *file, const char __user *buf, size_t count, loff_t *offset)
+static ssize_t neil_write(struct file *filp, const char __user *buf, size_t count, loff_t *offset)
 {
+	struct neil_device_data *p_data = filp->private_data;
 	uint8_t data[MAX_BUFFER_SIZE];
 	size_t size_to_copy, n_copied;
 
-	dev_info(device_data.drv_dev, "write device, minor: %d, count: %ld\n", MINOR(file->f_path.dentry->d_inode->i_rdev), count);
+	dev_info(p_data->drv_dev, "write device, minor: %d, count: %ld\n", MINOR(filp->f_path.dentry->d_inode->i_rdev), count);
 
 	if (count > MAX_BUFFER_SIZE)
 	{
 		size_to_copy = MAX_BUFFER_SIZE;
-		dev_info(device_data.drv_dev, "Only copy %d bytes from the user\n", MAX_BUFFER_SIZE);
+		dev_info(p_data->drv_dev, "only copy %d bytes from the user\n", MAX_BUFFER_SIZE);
 	}
 	else
 	{
 		size_to_copy = count;
-		dev_info(device_data.drv_dev, "Copy %ld bytes from the user\n", count);
+		dev_info(p_data->drv_dev, "copy %ld bytes from the user\n", count);
 	}
 
 	n_copied = copy_from_user(data, buf, size_to_copy);
 	if (n_copied < 0)
 	{
-		dev_err(device_data.drv_dev, "copy_from_user failed\n");
+		dev_err(p_data->drv_dev, "copy_from_user failed\n");
 		return -EACCES;
 	}
 
 	data[size_to_copy] = '\0';
 
-	dev_info(device_data.drv_dev, "copy_from_user: %s\n", data);
+	dev_info(p_data->drv_dev, "copy_from_user: %s\n", data);
 
 	return size_to_copy;
 }
 
 static __poll_t neil_poll(struct file *filp, struct poll_table_struct *wait)
 {
+	struct neil_device_data *p_data = filp->private_data;
 	__poll_t mask = 0;
 
-	dev_info(device_data.drv_dev, "poll_wait\n");
+	dev_info(p_data->drv_dev, "poll_wait\n");
 	poll_wait(filp, &neil_wait_queue, wait);
-	dev_info(device_data.drv_dev, "poll_wait exit\n");
+	dev_info(p_data->drv_dev, "poll_wait exit\n");
 
 	if (irq_occurred_flag)
 	{
@@ -181,63 +194,67 @@ static const struct file_operations neil_fops = {
 
 static int __init neil_init(void)
 {
-	if (alloc_chrdev_region(&device_data.dev, 0, 1, NEIL_DEVICE_NAME))
+	struct neil_device_data *p_data = &device_data;
+
+	if (alloc_chrdev_region(&p_data->dev, 0, 1, NEIL_DEVICE_NAME))
 	{
 		pr_err("alloc_chrdev_region failed\n");
 		return -1;
 	}
 
-	cdev_init(&device_data.cdev, &neil_fops);
-	if (cdev_add(&device_data.cdev, device_data.dev, 1) < 0)
+	cdev_init(&p_data->cdev, &neil_fops);
+	if (cdev_add(&p_data->cdev, p_data->dev, 1) < 0)
 	{
 		pr_err("cdev_add failed\n");
 		goto error_class;
 	}
 
-	device_data.drv_class = class_create(THIS_MODULE, NEIL_CLASS_NAME);
-	if (IS_ERR(device_data.drv_class))
+	p_data->drv_class = class_create(THIS_MODULE, NEIL_CLASS_NAME);
+	if (IS_ERR(p_data->drv_class))
 	{
 		pr_err("class_create failed\n");
 		goto error_class;
 	}
 #if UEVENT_ENABLE
-	device_data.drv_class->dev_uevent = neil_uevent;
+	p_data->drv_class->dev_uevent = neil_uevent;
 #endif
-	device_data.drv_dev = device_create(device_data.drv_class, NULL, device_data.dev, NULL, NEIL_DEVICE_NAME);
-	if (IS_ERR(device_data.drv_dev))
+	p_data->drv_dev = device_create(p_data->drv_class, NULL, p_data->dev, NULL, NEIL_DEVICE_NAME);
+	if (IS_ERR(p_data->drv_dev))
 	{
 		pr_err("device_create failed\n");
 		goto error_device;
 	}
 
-	if (request_irq(IRQ_NO, irq_handler, IRQF_SHARED, NEIL_DEVICE_NAME, (void *)irq_handler))
+	if (request_irq(IRQ_NO, irq_handler, IRQF_SHARED, NEIL_DEVICE_NAME, p_data))
 	{
 		pr_err("request_irq failed\n");
 		goto error_irq;
 	}
 
 	// initialize success
-	dev_info(device_data.drv_dev, "initialized. major: %d, minor: %d\n", MAJOR(device_data.dev), MINOR(device_data.dev));
+	dev_info(p_data->drv_dev, "initialized. major: %d, minor: %d\n", MAJOR(p_data->dev), MINOR(p_data->dev));
 	return 0;
 
 error_irq:
-	device_destroy(device_data.drv_class, device_data.dev);
+	device_destroy(p_data->drv_class, p_data->dev);
 error_device:
-	class_destroy(device_data.drv_class);
+	class_destroy(p_data->drv_class);
 error_class:
-	unregister_chrdev_region(device_data.dev, 1);
+	unregister_chrdev_region(p_data->dev, 1);
 	return -1;
 }
 
 static void __exit neil_exit(void)
 {
-	free_irq(IRQ_NO, (void *)irq_handler);
-	device_destroy(device_data.drv_class, device_data.dev);
-	class_destroy(device_data.drv_class);
-	cdev_del(&device_data.cdev);
-	unregister_chrdev_region(device_data.dev, 1);
+	struct neil_device_data *p_data = &device_data;
 
-	dev_info(device_data.drv_dev, "driver exited\n");
+	free_irq(IRQ_NO, p_data);
+	device_destroy(p_data->drv_class, p_data->dev);
+	class_destroy(p_data->drv_class);
+	cdev_del(&p_data->cdev);
+	unregister_chrdev_region(p_data->dev, 1);
+
+	dev_info(p_data->drv_dev, "exit\n");
 }
 
 module_init(neil_init);
@@ -246,4 +263,4 @@ module_exit(neil_exit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("neilchennc@gmail.com");
 MODULE_DESCRIPTION("Linux device driver example - AGX Orin power button detection");
-MODULE_VERSION("1.0");
+MODULE_VERSION("1.1");
